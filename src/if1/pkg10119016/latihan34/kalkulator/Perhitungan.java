0271285a300156d164164594556c800557ff56c7
aditi <aditi@DESKTOP-343N0FS>
2020-11-08 08:41:29 +0700

    Upload Project Tugas

---
+++ src/if1/pkg10119016/latihan34/kalkulator/Perhitungan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119016.latihan34.kalkulator;
+
+/**
+ *
+ * @author aditi
+ * NAMA              : Aditya Ilham Subagja
+ * KELAS             : IF1
+ * NIM               : 10119016
+ * Deskripsi Program : Program ini berisi program untuk menampilkan proses 
+ * penghitungan kalkulator
+ */
+public class Perhitungan {
+public double value1, value2;
+    
+    public double tambahBilangan() {return (value1+value2);}
+    public double kurangBilangan() {return (value1-value2);}
+    public double kaliBilangan() {return (value1*value2);}
+    public double bagiBilangan() {return (value1/value2);}
+    public double sisaBilangan() {return (value1%value2);}
+}
